785862ad8cc77c4f17f0682ab93c6efe946110f9
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-05-16 11:41:18 -0700

    Scala code

---
+++ containers/flink/master/src/main/scala/com/microsoft/chgeuer/ScalaJob.scala
@@ -0,0 +1,134 @@
+package com.microsoft.chgeuer
+
+// --topic.input test --topic.target results --group.id myGroup --bootstrap.servers localhost:9092 --zookeeper.connect localhost:2181
+
+// https://www.mapr.com/blog/essential-guide-streaming-first-processing-apache-flink
+// https://wwwvs.cs.hs-rm.de/vs-wiki/index.php/(SS16-03)_B%C3%B6rsendaten-Analyse_(Flink)
+// https://static.ph.ed.ac.uk/dissertations/hpc-msc/2015-2016/Triparna_Dasgupta-major_data_stream_processing_technologies_Triparna_Dasgupta.pdf
+
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.util.Collector
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
+import org.apache.flink.streaming.api.windowing.windows.Window
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer010, FlinkKafkaProducer010}
+import com.microsoft.chgeuer.proto.messages.{Calculated, Point, TrackingPacket, TripAggregation}
+
+object ScalaJob extends App {
+  // val args2 = "--topic.input test --topic.target results --group.id myGroup --bootstrap.servers localhost:9092 --zookeeper.connect localhost:2181".split(" +")
+  // val env = StreamExecutionEnvironment.createLocalEnvironment(parallelism = 1)
+
+  val params = ParameterTool.fromArgs(args)
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  env.getConfig.setGlobalJobParameters(params)
+  env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+  val simulatedStream: DataStream[TrackingPacket] = env.addSource(
+    new TrackingPacketSource)
+
+  /*
+  val kapkaInputStream: DataStream[TrackingPacket] = env.addSource(
+    new FlinkKafkaConsumer010[TrackingPacket](
+      params.getRequired("topic.input"),
+      new TrackingPacketDeserializer,
+      params.getProperties
+    )
+  )
+  */
+
+  var rawStream = simulatedStream
+
+  val rawStreamWithTimestamps: DataStream[TrackingPacket] = rawStream
+    .assignTimestampsAndWatermarks(
+      new BoundedOutOfOrdernessTimestampExtractor[TrackingPacket](Time.seconds(0)) {
+        override def extractTimestamp(element: TrackingPacket): Long = element.millisecondsSinceEpoch
+      }
+    )
+
+  val keyed: KeyedStream[TrackingPacket, (Long, Int)] = rawStreamWithTimestamps
+    .keyBy(x => (x.ccn, x.tripid))
+
+  val reduced_new: DataStream[TripAggregation] = keyed
+    // .countWindow(size = 5)
+    .window(EventTimeSessionWindows.withGap(Time.seconds(2))) // .allowedLateness(Time.seconds(2))
+    .apply((key: (Long, Int), window: Window, input: Iterable[TrackingPacket], out: Collector[TripAggregation]) => {
+      val ccn = key._1
+      val tripid = key._2
+
+      if (input.nonEmpty) {
+        val pointList = input.map(tp => Point(
+          millisecondsSinceEpoch = tp.millisecondsSinceEpoch,
+          lat = tp.lat, lon = tp.lon))
+
+        val data = helper.combinePoints(pointList)
+
+        out.collect(TripAggregation(ccn = ccn, tripid = tripid, data = data))
+      }
+    }
+  )
+
+  // reduced_new.print()
+
+  val kafkaSink = FlinkKafkaProducer010.writeToKafkaWithTimestamps[TripAggregation](
+    reduced_new.javaStream,
+    params.getRequired("topic.target"),
+    new TripAggregationSerializationSchema,
+    params.getProperties
+  )
+  kafkaSink.setLogFailuresOnly(false)
+  kafkaSink.setFlushOnCheckpoint(true)
+
+  env.execute("Christian's ScalaJob")
+}
+
+
+object ScalaJobProd extends App {
+  val params = ParameterTool.fromArgs(args)
+
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  env.getConfig.setGlobalJobParameters(params)
+  env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+  val processedStream = env.addSource(
+      new FlinkKafkaConsumer010[TrackingPacket](
+        params.getRequired("topic.input"),
+        new TrackingPacketDeserializer,
+        params.getProperties
+      )
+    )
+    .keyBy(x => (x.ccn, x.tripid))
+    // .countWindow(size = 10)
+    // .window(EventTimeSessionWindows.withGap(Time.seconds(2))).allowedLateness(Time.seconds(2))
+    .window(EventTimeSessionWindows.withGap(Time.seconds(2)))
+    .apply((key: (Long, Int), window: Window, input: Iterable[TrackingPacket], out: Collector[TripAggregation]) => {
+        val ccn = key._1
+        val tripid = key._2
+        if (input.nonEmpty) {
+          val pointList = input.map(tp => Point(millisecondsSinceEpoch = tp.millisecondsSinceEpoch, 
+                                                lat = tp.lat, lon = tp.lon))
+          val data = helper.combinePoints(pointList)
+          out.collect(TripAggregation(ccn = ccn, tripid = tripid, data = data))
+        }
+      }
+    )
+    .javaStream
+
+  val kafkaSink = FlinkKafkaProducer010.writeToKafkaWithTimestamps[TripAggregation](
+    processedStream,
+    params.getRequired("topic.target"),
+    new TripAggregationSerializationSchema,
+    params.getProperties
+  )
+  kafkaSink.setLogFailuresOnly(false)
+  kafkaSink.setFlushOnCheckpoint(true)
+
+  env.execute()
+}
+
+// Enrichment:
+// - could do async I/O to join on location DB
+// - send stuff to Kafka and have somebody else deal with it
+// - radical: Everything is a stream. Push all possible locations into cluster memory and join on the streams
---
+++ containers/flink/master/src/main/scala/com/microsoft/chgeuer/helper.scala
@@ -0,0 +1,56 @@
+package com.microsoft.chgeuer
+
+import scala.math._
+import scala.collection.mutable.ListBuffer
+import com.microsoft.chgeuer.proto.messages.{Calculated, Point}
+
+object helper {
+
+  def haversineInMeters (lat1:Double, lon1:Double, lat2:Double, lon2:Double) :Double = {
+    val R = 6372.8e3
+    val dLat=(lat2 - lat1).toRadians
+    val dLon=(lon2 - lon1).toRadians
+    val a = pow(sin(dLat/2),2) + pow(sin(dLon/2), 2) * cos(lat1.toRadians) * cos(lat2.toRadians)
+    val c = 2 * asin( min(1.0, sqrt(a))   )
+    R * c
+  }
+
+  def combinePoints (points: Iterable[Point]) : ListBuffer[Point] = {
+    val pointList = points.toList
+      .sortBy(_.millisecondsSinceEpoch)
+
+    var data : ListBuffer[Point] = ListBuffer[Point]()
+    for (i <- pointList.indices) {
+      val curr = pointList(i)
+      var calculatedProperties : Option[Calculated] = if (i == 0) {
+        Console.out.println(s"No speed available, start of window")
+        None
+      } else {
+        val prev = pointList(i - 1)
+
+        val seconds: Double = (curr.millisecondsSinceEpoch - prev.millisecondsSinceEpoch) / 1000.0
+        Console.out.println(s"Seconds $seconds curr=${curr.millisecondsSinceEpoch}ms prev=${prev.millisecondsSinceEpoch}ms")
+        val meters = helper.haversineInMeters(prev.lat, prev.lon, curr.lat, curr.lon)
+        val kmh = 3.6 * meters / seconds
+
+        if (kmh > 300.0) {
+          Console.out.println(s"Speed too high, discarding data point: ${kmh.formatted("%.1f")} km/h")
+          // None
+          Some(Calculated(timeDifferenceToPreviousPoint = seconds, distanceInMetersToPreviousPoint = meters))
+        }
+        else {
+          Console.out.println(s"Valid speed: ${kmh.formatted("%.1f")} km/h")
+          Some(Calculated(timeDifferenceToPreviousPoint = seconds, distanceInMetersToPreviousPoint = meters))
+        }
+      }
+
+      Console.out.println(s"Adding point ${calculatedProperties}")
+      data = data :+ Point(
+        millisecondsSinceEpoch = curr.millisecondsSinceEpoch,
+        lat = curr.lat, lon = curr.lon,
+        properties = calculatedProperties)
+    }
+
+    data
+  }
+}
