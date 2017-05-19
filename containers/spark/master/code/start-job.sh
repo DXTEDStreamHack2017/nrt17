e155a634f2dc2c2b040b8d3eabddbd49fc69a416
benjguin <benjamin@3-4.fr>
2017-05-18 17:01:19 -0700

    fixed sparkm

--- containers/spark/master/code/start-job.sh
+++ containers/spark/master/code/start-job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 hdfs dfs -rm /clustervolume/*
-hdfs dfs -copyFromLocal boontadata-spark-job1-assembly-0.1.jar /clustervolume
+hdfs dfs -copyFromLocal code/target/scala-2.11/nrt17-spark-job1-assembly-0.1.jar /clustervolume
 
 spark-submit \
     --class com.benjguin.nrt17.sparkjob1.Main \
     --deploy-mode cluster \
     --master spark://sparkm1:6066 \
-    /clustervolume/boontadata-spark-job1-assembly-0.1.jar \
+    /clustervolume/nrt17-spark-job1-assembly-0.1.jar \
     ks1:9092,ks2:9092,ks3:9092 sampletopic
